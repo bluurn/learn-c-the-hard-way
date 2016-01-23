fc36fde1756d5b2b0a99c6ca414c2f8d223f0787
v.suvorov <v.suvorov@fun-box.ru>
2016-01-23 12:19:08 +0300

    ex10

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 FLAGS=-Wall -g
 
-all : clean ex1/ex1 ex3/ex3 ex5/ex5 ex6/ex6 ex7/ex7 ex8/ex8 ex9/ex9
+all : clean ex1/ex1 ex3/ex3 ex5/ex5 ex6/ex6 ex7/ex7 ex8/ex8 ex9/ex9 ex10/ex10
 
 clean:
 			rm -rf ex1/ex1 ex1/ex1.dSYM
@@ -10,6 +10,7 @@ clean:
 			rm -rf ex7/ex7 ex7/ex7.dSYM
 			rm -rf ex8/ex8 ex8/ex8.dSYM
 			rm -rf ex9/ex9 ex9/ex9.dSYM
+			rm -rf ex10/ex10 ex10/ex10.dSYM
 
 
 .PHONY: all clean
---
+++ ex10/ex10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  // loop thru args, passed to program
+  for(int i = 0; i < argc; i++) {
+    printf("arg %d: %s\n", i, argv[i]);
+  }
+  // arr of strings
+
+  char *states[] = {
+    "California",
+    "Oregon",
+    "Washington",
+    "Texas"
+  };
+
+  int num_states = 4;
+
+  for(int i = 0; i < num_states; i++) {
+    printf("state %d: %s\n", i, states[i]);
+  }
+
+  return 0;
+}
