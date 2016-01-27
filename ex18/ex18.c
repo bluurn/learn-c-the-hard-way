a6017bfa9fd7901809e71a04ad1b4e263e39a0d0
Vladimir Suvorov <v.suvorov@fun-box.ru>
2016-01-27 11:14:46 +0300

    ex18

--- ex18/ex18.c
+++ ex18/ex18.c
@@ -17,6 +17,61 @@ void die(const char *message) {
 
 typedef int (*compare_cb)(int a, int b);
 
+int *bubble_sort(int *numbers, int count, compare_cb cmp) {
+  int temp = 0;
+  int *target = malloc(count * sizeof(int));
+
+  if(!target) {
+    die("Memory error.");
+  }
+
+  memcpy(target, numbers, count * sizeof(int));
+  
+  for(int i = 0; i < count; i++) {
+    for(int j = 0; j < count - 1; j++) {
+      if(cmp(target[j], target[j+1]) > 0) {
+        temp = target[j+1];
+        target[j+1] = target[j];
+        target[j] = temp;
+      }
+    }
+  }
+
+  return target;
+}
+
+int sorted_order(int a, int b) {
+  return a - b;
+}
+
+int reverse_order(int a, int b) {
+  return b - a;
+}
+
+int strange_order(int a, int b) {
+  if(a == 0 || b == 0) {
+    return 0;
+  } else {
+    return a % b;
+  }
+}
+
+void test_sorting(int *numbers, int count, compare_cb cmp) {
+
+  int *sorted = bubble_sort(numbers, count, cmp);
+
+  if(!sorted) {
+    die("Failed to sort");
+  }
+
+  for(int i = 0; i < count; i++) {
+    printf("%d ", sorted[i]);
+  }
+  printf("\n");
+
+  free(sorted);
+}
+
 int main(int argc, char *argv[]) {
   if(argc < 2) {
     die("USAGE: ex18 3 4 1 2");
@@ -32,6 +87,10 @@ int main(int argc, char *argv[]) {
   for(int i = 0; i < count; i++) {
     numbers[i] = atoi(inputs[i]);
   }
+
+  test_sorting(numbers, count, sorted_order);
+  test_sorting(numbers, count, reverse_order);
+  test_sorting(numbers, count, strange_order);
 
   free(numbers);
 
